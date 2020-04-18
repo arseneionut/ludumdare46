406b4939386f6b2027cde9adb4b8410059df4c1b
Cristian Rizea <40300870+cristianrizea@users.noreply.github.com>
2020-04-18 18:56:00 +0300

    play sound

---
+++ godot/scenes/Audio_pl.gd
@@ -0,0 +1,23 @@
+
+
+#class Audio_pl:
+#
+#	var player
+#
+#	func _init():
+#		player = AudioStreamPlayer.new()
+#
+#
+#	func play_sound(snd_res):
+#		player.stream = load(snd_res)
+#		player.play()
+
+
+
+#func _ready():
+#	var player = AudioStreamPlayer.new()
+#	self.add_child(player)
+#	player.stream = load("res://resources/Sounds/Yes.wav")
+#	player.play()
+#	pass # Replace with function body.
+
