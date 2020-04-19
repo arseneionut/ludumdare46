e3015c32a877d1dd217cd7d81de23d9d57a70774
Miron Brezuleanu <mbrezu@gmail.com>
2020-04-19 14:37:31 +0300

    Explosions and lasers.

---
+++ godot/scenes/explosion.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_sprite_animation_finished():
+	queue_free()
---
+++ godot/scenes/lasers.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var counter = 2
+
+func _on_sprite_animation_finished():
+	counter -= 1
+	if counter <= 0:
+		queue_free()
